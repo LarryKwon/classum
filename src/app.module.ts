217a2e57b058d26a2026f0a0556aff60e45fc911
larrykwon <koallarry11@kaist.ac.kr>
2022-05-20 16:55:29 +0900

    domain별 모듈 추가(auth, user, space, spaceRole, chat) production과 development 환경 다르도록 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { SpaceModule } from './space/space.module';
+import { SpaceRoleModule } from './space-role/space-role.module';
+import { PostModule } from './post/post.module';
+import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    SpaceModule,
+    SpaceRoleModule,
+    PostModule,
+    ChatModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      load: [config],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
