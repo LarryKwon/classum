e81097425cfe54df31948a52ab16ac4ffcbe7017
larrykwon <koallarry11@kaist.ac.kr>
2022-06-06 01:05:49 +0900

    Create, Read에 대한 api 구현

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ChatService } from './chat.service';
+import { PoliciesGuard } from '../auth/guards/policies.guard';
+import { ReadChatPolicyHandler } from '../auth/guards/policy-handler/chat/chat.read-policy.handler';
+import { CheckPolicies } from '../auth/decorator/policy.decorator';
+import { CreateChatPolicyHandler } from '../auth/guards/policy-handler/chat/chat.create-policy.handler';
+import { UpdateChatPolicyHandler } from '../auth/guards/policy-handler/chat/chat.update-policy.handler';
+import { DeleteChatPolicyHandler } from '../auth/guards/policy-handler/chat/chat.delete-policy.handler';
+import { FindChatsDto } from './dto/find-chats.dto';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { CreateReplyDto } from './dto/create-reply.dto';
+import { UpdateChatDto } from './dto/update-chat.dto';
+import { DeleteChatDto } from './dto/delete-chat.dto';
+import { GetUser } from '../auth/decorator/get-user.decorator';
+import { User } from '../user/entity/user.entity';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
+
+  @Get()
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new ReadChatPolicyHandler())
+  findAllChats(findChatsDto: FindChatsDto, @GetUser() user: User) {
+    return this.chatService.findAllChats(findChatsDto, user);
+  }
+
+  @Post('/reply')
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new CreateChatPolicyHandler())
+  createReply(createReplyDto: CreateReplyDto, @GetUser() user: User) {
+    return this.chatService.createReply(createReplyDto, user);
+  }
+
+  @Post()
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new CreateChatPolicyHandler())
+  createChat(createChatDto: CreateChatDto, @GetUser() user: User) {
+    return this.chatService.createChat(createChatDto, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new UpdateChatPolicyHandler())
+  updateChat(updateChatDto: UpdateChatDto) {
+    return this.chatService.updateChat(updateChatDto);
+  }
+
+  @Delete('/:id')
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies(new DeleteChatPolicyHandler())
+  deleteChat(deleteChatDto: DeleteChatDto) {
+    return this.chatService.deleteChat(deleteChatDto);
+  }
+}
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,107 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChatRepository } from './repository/chat.repository';
+import { CaslAbilityFactory } from '../casl/casl-ability.factory';
+import { PostService } from '../post/post.service';
+import { ChatTreeRepository } from './repository/chat.tree-repository';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { CreateReplyDto } from './dto/create-reply.dto';
+import { UpdateChatDto } from './dto/update-chat.dto';
+import { DeleteChatDto } from './dto/delete-chat.dto';
+import { User } from '../user/entity/user.entity';
+import { SpaceService } from '../space/space.service';
+import { Action } from '../auth/enum/Action';
+import { FindChatsDto } from './dto/find-chats.dto';
 
 @Injectable()
-export class ChatService {}
+export class ChatService {
+  constructor(
+    @InjectRepository(ChatRepository)
+    private readonly chatRepository: ChatRepository,
+    @InjectRepository(ChatTreeRepository)
+    private readonly chatTreeRepository: ChatTreeRepository,
+    private readonly caslAbilityFactory: CaslAbilityFactory,
+    private readonly postService: PostService,
+    private readonly spaceService: SpaceService,
+  ) {}
+
+  async findChatById(id: number) {
+    return await this.chatTreeRepository.findOneOrFail(id);
+  }
+
+  async findAllChats(findChatsDto: FindChatsDto, user: User) {
+    const { spaceId, postId } = findChatsDto;
+    const space = await this.spaceService.findSpaceById(spaceId);
+    const post = await this.postService.findPostById(postId);
+
+    const ability = await this.caslAbilityFactory.createForUser(user, space);
+    if (ability.can(Action.Read, post)) {
+      return post.chats;
+    } else {
+      throw new ForbiddenException(
+        `can't see chats on posts in space that you are not participated in`,
+      );
+    }
+  }
+
+  async createChat(createChatDto: CreateChatDto, user: User) {
+    const { contents, isAnonymous, spaceId, postId } = createChatDto;
+    const space = await this.spaceService.findSpaceById(spaceId);
+    const post = await this.postService.findPostById(postId);
+    const createdChat = await this.chatRepository.create({
+      writer: user,
+      contents: contents,
+      isAnonymous: isAnonymous,
+      post: post,
+    });
+    const ability = await this.caslAbilityFactory.createForUser(user, space);
+    // role에 따라 anonymous로 작성할 수 있는지 & 해당 post에 read 권한이 있는지
+    if (
+      ability.can(Action.Create, createdChat) &&
+      ability.can(Action.Read, post)
+    ) {
+      return await this.chatRepository.save(createdChat);
+    } else {
+      throw new BadRequestException(
+        `manager can't create chat with anonymous options`,
+      );
+    }
+  }
+
+  async createReply(createReplyDto: CreateReplyDto, user: User) {
+    const { contents, isAnonymous, spaceId, postId, parentId } = createReplyDto;
+    const post = await this.postService.findPostById(postId);
+    const space = await this.spaceService.findSpaceById(spaceId);
+    const parentChat = await this.findChatById(parentId);
+    const createdChat = await this.chatRepository.create({
+      writer: user,
+      contents: contents,
+      isAnonymous: isAnonymous,
+      post: post,
+      parent: parentChat,
+    });
+    const ability = await this.caslAbilityFactory.createForUser(user, space);
+    // role에 따라 anonymous로 작성할 수 있는지 & 해당 post에 read 권한이 있는지
+    if (
+      ability.can(Action.Create, createdChat) &&
+      ability.can(Action.Read, post)
+    ) {
+      return await this.chatRepository.save(createdChat);
+    } else {
+      throw new BadRequestException(
+        `manager can't create chat with anonymous options`,
+      );
+    }
+  }
+
+  async updateChat(updateChatDto: UpdateChatDto) {
+    return null;
+  }
+  async deleteChat(deleteChatDto: DeleteChatDto) {
+    return null;
+  }
+}
